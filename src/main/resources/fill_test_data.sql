96d6e6c6a617da6c5d4dcc88a6b6a1a6a4643dec
Aleksandr Saushev <aleksandr_saushev@epam.com>
2022-03-22 10:39:10 +0000

    Sql file added

---
+++ src/main/resources/fill_test_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO 
+	users (id, name, surname, balance)
+VALUES 
+	(1, 'Alfredo', 'Pacino', 131234123.00),
+	(2, 'Quentin', 'Tarantino', 98754321.00),
+	(3, 'Martin', 'Scorsese', 2341.23);
