1f446f436daa1d6e6626cee45f1a42dc59867f58
Mikhail Ratnikov <mikhail_ratnikov@epam.com>
2022-03-22 12:06:05 +0000

    EPMLSTRURA-39: extend db schema

--- src/main/resources/V1__init_db.sql
+++ src/main/resources/V1__init_db.sql
@@ -1,6 +1,17 @@
-CREATE TABLE users (
-    id integer NOT NULL PRIMARY KEY,
-    name character varying(50) NOT NULL,
-    surname character varying(50) NOT NULL,
-    balance decimal(15,2) NOT NULL
+CREATE TABLE users
+(
+    id INTEGER PRIMARY KEY,
+    name CHARACTER VARYING(50) NOT NULL,
+    surname CHARACTER VARYING(50) NOT NULL,
+    balance Decimal(15,2) NOT NULL,
+    CHECK ((Name != '') AND (Surname != ''))
 );
+
+CREATE TABLE transactions
+(
+    id SERIAL PRIMARY KEY,
+    userid INTEGER REFERENCES Users (id),
+    type CHARACTER VARYING (50) NOT NULL,
+    amount DECIMAL (15,2) NOT NULL,
+    time TIMESTAMPTZ NOT NULL
+)
